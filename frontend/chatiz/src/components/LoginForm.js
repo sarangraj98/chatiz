d7fc11f16d9b76199d9659706b36f62561d9613f
sarangr <sarangr@qburst.com>
2021-03-31 16:42:12 +0530

    #1-Added styling for login

--- frontend/chatiz/src/components/LoginForm.js
+++ frontend/chatiz/src/components/LoginForm.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import '../styles/loginform.css'
 export default function LoginForm() {
-    const submitHandler = ()=>{
+    const submitHandler = () => {
         alert('Button clicked')
     }
     return (
         <div className="container">
-            <div className="card">
-                <h1>Enter the chat room !</h1>
-                <input name="name" placeholder="Enter your name" required></input>
-                <br/><select>
-                    <option value="">Test</option>
-                    <option value="">Test</option>
-                    <option value="">Test</option>
-                </select><br/>
-                <button onClick={submitHandler}>ENTER</button>
+            <div className="card form">
+                <h1 style={{ textAlign: 'center' }}>Enter the chat room !</h1>
+                <form className="login-form">
+                    <input type="text" placeholder="username" />
+                    <input type="password" placeholder="password" />
+                    <button onClick={submitHandler}>login</button>
+                </form>
             </div>
         </div>
     )
