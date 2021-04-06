f03b52ead13d1bebf56c710920249b25ee4a712e
sarangr <sarangr@qburst.com>
2021-04-06 11:43:05 +0530

    #3_added home page and dashboard routing

--- frontend/chatiz/src/components/LoginForm.js
+++ frontend/chatiz/src/components/LoginForm.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import '../styles/loginform.css'
-export default function LoginForm() {
-    const submitHandler = () => {
-        alert('Button clicked')
-    }
+import auth from '../utils/auth'
+export default function LoginForm(props) {
     return (
         <div className="container">
             <div className="card form">
                 <h1 style={{ textAlign: 'center' }}>Enter the chat room !</h1>
-                <form className="login-form">
+                
                     <input type="text" placeholder="username" />
                     <input type="password" placeholder="password" />
-                    <button onClick={submitHandler}>login</button>
-                </form>
+                    <button onClick={()=>{
+                        auth.login(()=>{
+                            props.history.push("/dashboard")
+                        })
+                    }}>login</button>
+                
             </div>
         </div>
     )
